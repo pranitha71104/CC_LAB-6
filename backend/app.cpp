7a6efeaf5b1268d7501d8c5954a3159a00be6343
pranitha71104 <pranitha71104@gmail.com>
2026-03-07 20:28:56 +0530

    Refactor socket handling and improve response format

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
+#include <cstdlib>
 #include <unistd.h>
-#include <string.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 
 int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-
+    sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(80);
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 3);
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd, 5);
+
+    char hostname[1024];
+    gethostname(hostname, 1024);
 
     while (true) {
-        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        int client = accept(server_fd, NULL, NULL);
 
         std::string response =
-        "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nServed by backend container\n";
+        "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nServed by backend: ";
+        response += hostname;
 
-        send(new_socket, response.c_str(), response.size(), 0);
-        close(new_socket);
+        send(client, response.c_str(), response.size(), 0);
+        close(client);
     }
+
+    return 0;
 }
