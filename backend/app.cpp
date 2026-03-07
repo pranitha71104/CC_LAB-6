3a87ce84b5b344a9c34078da62501048b49438f8
pranitha71104 <pranitha71104@gmail.com>
2026-03-07 18:48:41 +0530

    Add basic HTTP server functionality
    Implement a basic HTTP server that responds to requests.

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(80);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
     while (true) {
-        std::cout << "Served by backend: " << getpid() << std::endl;
-        sleep(5);
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+        std::string response =
+        "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nServed by backend container\n";
+
+        send(new_socket, response.c_str(), response.size(), 0);
+        close(new_socket);
     }
-    return 0;
 }
